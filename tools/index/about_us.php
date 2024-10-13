08f4c0b4cb4c88c48d23dc7914f3f21304881d96
Brunobrno <brunovontor@gmail.com>
2024-10-13 23:46:48 +0200

    fwfeefsfesfes

--- tools/index/about_us.php
+++ tools/index/about_us.php
@@ -6,7 +6,7 @@
         </header>
 
         <main>
-            <p>V oblasti reklamy jsme doma od roku 1994. Jsme moderní a kreativní tým, který neustále sleduje aktuální trendy v oboru a snažíme se rozšiřovat naši nabídku služeb tak, abychom vyhověli všem vašim požadavkům.</p>
+            <p>V oblasti reklamy jsme doma od roku 1994.</p>
 
             <h2>Naše specializace</h2>
             <p>Od návrhu až po realizaci se staráme o kompletní grafickou prezentaci firem. Máme vlastní výrobní zázemí, což nám umožňuje rychlé zpracování zakázek. Díky naší situaci v centru města jsme vždy blízko k vám.</p>
@@ -28,10 +28,18 @@
         </header>
 
         <main>
-            <p>V oboru reklamy od roku 1994.</p>
-            <p>Moderní a kreativní tým. </p>
-            <p>Specializace na grafickou prezentaci a tisk firemních materiálů. </p>
-            <p>Cíl: Udržet konkurenceschopné ceny a kvalitu.</p>
+        <p>V oblasti reklamy jsme doma od roku 1994.</p>
+
+        <h2>Naše specializace</h2>
+        <p>Od návrhu až po realizaci se staráme o kompletní grafickou prezentaci firem. Máme vlastní výrobní zázemí, což nám umožňuje rychlé zpracování zakázek. Díky naší situaci v centru města jsme vždy blízko k vám.</p>
+
+        <h2>Široká škála služeb</h2>
+        <p>Nabízíme komplexní grafické zpracování a tisk firemních materiálů. Od vizitek a pozvánek, přes hlavičkové papíry až po speciální formuláře, reklamní letáky, propagační materiály a katalogy. Specializujeme se také na Signmaking, což zahrnuje výrobu reklamy od samolepek přes světelnou reklamu až po bigboardy.</p>
+
+        <h2>Naše přednosti</h2>
+        <p>Jsme hrdí na mnoho úspěšných projektů, které pomohly našim klientům k jednotné identitě firmy a zviditelnění na trhu. Naším hlavním cílem je udržet konkurenceschopné ceny a zůstat kvalitním a cenově dostupným partnerem pro naše klienty.</p>
+
+        <p>S námi máte jistotu, že vaše reklamní potřeby budou v dobrých rukou.</p>
         </main>
     </div>
 </div>
