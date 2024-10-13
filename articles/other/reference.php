442c0e8bd7da16b9defba6e2e4281a90bdf38e21
Brunobrno <brunovontor@gmail.com>
2024-10-13 16:22:14 +0200

    idk

--- articles/other/reference.php
+++ articles/other/reference.php
@@ -2,6 +2,7 @@
     <header>
         <h1>Reference</h1>
     </header>
-
+    <img class="desktop-img" src="/img/other/reference/reference_nasirku.svg" alt="reference firem">
+    <img class="mobile-img" src="/img/other/reference/reference_navysku.svg" alt="reference firem">
     
 </article>
